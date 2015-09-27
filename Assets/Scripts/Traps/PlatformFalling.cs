a1664905f78e2d726926ed7bbe6cc77176862035
krixalis <krixdev@gmail.com>
2015-09-27 04:17:58 +0200

    Make falling platforms wait before falling. Migrate back to github from gitlab. Old changes: -Simple falling platform. -Receiving DoubleJump PowerUp does not automatically trigger a jump anymore.

--- Assets/Scripts/Traps/PlatformFalling.cs
+++ Assets/Scripts/Traps/PlatformFalling.cs
@@ -13,20 +13,21 @@ public class PlatformFalling : MonoBehaviour
 	
 	// Update is called once per frame
 	void FixedUpdate ()
-    {
+	{
+	    if (_startTime != 0f && Time.time - _startTime >= 2f) StartFalling = true;
 	    if (StartFalling)
 	    {
 	        transform.root.GetComponent<Rigidbody>().isKinematic = false;
-            transform.root.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, -50f));
+            transform.root.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, -35f));
+	        transform.root.GetComponent<BoxCollider>().enabled = false;
 	    }
 	}
 
     public void OnTriggerEnter(Collider otherCollider)
     {
-        if (otherCollider.tag == "Player")
+        if (otherCollider.tag == "Player" && _startTime == 0f)
         {
             _startTime = Time.time;
-            StartFalling = true;
         }
     }
 }
