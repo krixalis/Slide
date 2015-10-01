2f69c19df5c89d90a75655cdb75ac99567c54232
krixalis <krixdev@gmail.com>
2015-10-01 21:03:27 +0200

    Player can move down one-way platforms by pressing down, now. testScene changed.

--- Assets/PlatformOneWay.cs
+++ Assets/PlatformOneWay.cs
@@ -18,5 +18,11 @@ public class PlatformOneWay : MonoBehaviour
         _playerFeetPosY = Player.transform.position.y - (Player.transform.localScale.y / 2); //player position changes, therefore we need to update the feet position here
 	    
 	    Physics.IgnoreCollision(GetComponent<Collider>(), Player.GetComponent<Collider>(), transform.position.y > _playerFeetPosY);
+
+	    if (Input.GetAxis("Vertical") < 0)
+	    {
+	        Physics.IgnoreCollision(GetComponent<Collider>(), Player.GetComponent<Collider>(), true); 
+	        
+	    }
     }
 }
